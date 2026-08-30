696dddb695c895df5384d96f7a35332a0810b14f
Ariq Isaac <2009ariq@gmail.com>
2026-08-30 19:06:07 +0800

    Create playRound() Function that plays a single round of rock paper scissors

--- main.js
+++ main.js
@@ -8,20 +8,75 @@ function getComputerChoice() {
         return 'rock';
     }
     else if (choice === 1) {
-        return 'paper'
+        return 'paper';
     }
     else {
-        return 'scissors'
+        return 'scissors';
     }
 }
 
 // Create a function that takes an input from the human player and return it
 function getHumanChoice() {
     let choice = prompt("Enter 'rock', 'paper' or 'scissors'").toLowerCase();
-    return choice
+    return choice;
 }
 
 // create a variable named humanScore and initialize with the value of 0
+let humanScore = 0;
 // create a variable named computerScore and initialize with the value of 0
-// Create a function that compares both human's and computer's input.
-// increments humanScore or computerScore depending who won
+let computerScore = 0;
+
+// Create a function that plays a single round AND THEN compares both human's and computer's input.
+function playRound(humanChoice, computerChoice) {
+
+    if (computerChoice === 'rock') {
+        if (humanChoice === 'paper') {
+            console.log("You win! Paper beats Rock");
+            humanScore++;
+        }
+        else if (humanChoice === 'scissors') {
+            console.log("You lose! Rock beats Scissors");
+            computerScore++;
+        }
+        else {
+            console.log("It's a draw!")
+        }
+    }
+
+    else if (computerChoice === 'paper') {
+        if (humanChoice === 'rock') {
+            console.log("You lose! Paper beats Rock");
+            computerScore++;
+        }
+        else if (humanChoice === 'scissors') {
+            console.log("You win! Scissors beats paper")
+            humanScore++;
+        }
+        else {
+            console.log("It's a draw!");
+        }
+    }
+
+    else {
+        if (humanChoice == 'rock') {
+            console.log("You win! Rock beats Scissors");
+            humanChoice++;
+        }
+        else if (humanChoice === 'paper') {
+            console.log("You lose! Scissors beats Paper");
+            computerChoice++;
+        }
+        else {
+            console.log("It's a draw!");
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+const result = playRound(humanSelection, computerSelection);
+
+console.log(result);
+
+// Create a function that plays an entire 5-round game
