ea4f462fa470a393fc9d92c99841157b33a51dab
Ariq Isaac <2009ariq@gmail.com>
2026-08-30 18:24:35 +0800

    Create getComputerChoice() Function that randomly returns rock, paper, or scissors

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("Hello World")
+// Create a function that randomly returns 'rock' , 'paper' or 'scissors'
+function getComputerChoice() {
+    let choice = Math.round(Math.random() * 3);
+    if (choice === 0) {
+        return 'rock';
+    }
+    else if (choice === 1) {
+        return 'paper'
+    }
+    else {
+        return 'scissors'
+    }
+}
+
+console.log(getComputerChoice());
+
+// Create a function that takes an input from the human player and return it
+// create a variable named humanScore and initialize with the value of 0
+// create a variable named computerScore and initialize with the value of 0
+// Create a function that compares both human's and computer's input.
+// increments humanScore or computerScore depending who won
