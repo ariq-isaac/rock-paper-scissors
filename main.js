68977d76803779e72083cc45a0cc63c080c801c3
Ariq Isaac <2009ariq@gmail.com>
2026-08-30 18:38:21 +0800

    Create getHumanChoice() function that takes an input from user

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // Create a function that randomly returns 'rock' , 'paper' or 'scissors'
 function getComputerChoice() {
-    let choice = Math.round(Math.random() * 3);
+
+    // Gets a random number between 0 , 1, or 3
+    // Returns 'rock', 'paper' or 'scissors' depending on the numbers
+    let choice = Math.floor(Math.random() * 3);
     if (choice === 0) {
         return 'rock';
     }
@@ -12,9 +15,12 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
-
 // Create a function that takes an input from the human player and return it
+function getHumanChoice() {
+    let choice = prompt("Enter 'rock', 'paper' or 'scissors'").toLowerCase();
+    return choice
+}
+
 // create a variable named humanScore and initialize with the value of 0
 // create a variable named computerScore and initialize with the value of 0
 // Create a function that compares both human's and computer's input.
